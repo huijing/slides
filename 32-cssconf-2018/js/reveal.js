0e67f9e1fe7f602f460c09dca34a5f139cff4431
Chen Hui Jing <huijing.chen@jewelpaymentech.com>
2018-05-22 11:43:27 +0300

    WIP cssconf slides

--- 32-cssconf-2018/js/reveal.js
+++ 32-cssconf-2018/js/reveal.js
@@ -39,8 +39,8 @@
 
 			// The "normal" size of the presentation, aspect ratio will be preserved
 			// when the presentation is scaled to fit different resolutions
-			width: 1280,
-			height: 720,
+			width: 1920,
+			height: 1080,
 
 			// Factor of the display size that should remain empty around the content
 			margin: 0.04,
