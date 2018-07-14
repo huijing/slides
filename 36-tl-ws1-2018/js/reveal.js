01d508e5ede690554145c14716d8e22ce5d3a292
huijing <kakyou_tensai@yahoo.com>
2018-07-14 15:10:38 +0800

    Update techladies workshop slides

--- 36-tl-ws1-2018/js/reveal.js
+++ 36-tl-ws1-2018/js/reveal.js
@@ -39,8 +39,8 @@
 
 			// The "normal" size of the presentation, aspect ratio will be preserved
 			// when the presentation is scaled to fit different resolutions
-			width: 960,
-			height: 700,
+			width: 1600,
+			height: 900,
 
 			// Factor of the display size that should remain empty around the content
 			margin: 0.04,
