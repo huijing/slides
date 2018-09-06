d46b8d8210a796cf43ba20fb0ccf460c48d89399
huijing <kakyou_tensai@yahoo.com>
2018-09-06 22:22:27 +0300

    Adjust resolution for refresh slides

--- 39-refresh-2018/js/reveal.js
+++ 39-refresh-2018/js/reveal.js
@@ -39,8 +39,8 @@
 
 			// The "normal" size of the presentation, aspect ratio will be preserved
 			// when the presentation is scaled to fit different resolutions
-			width: 1600,
-			height: 900,
+			width: 1920,
+			height: 1080,
 
 			// Factor of the display size that should remain empty around the content
 			margin: 0.04,
