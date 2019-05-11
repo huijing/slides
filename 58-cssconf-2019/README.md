f7e178a871821e9e66952c5c424a61394eed56ac
Chen Hui Jing <kakyou_tensai@yahoo.com>
2019-05-11 08:49:43 +0800

    Finish draft of YGLF slides

---
+++ 58-cssconf-2019/README.md
@@ -0,0 +1,3 @@
+# The layout triple-team
+
+Pared-down examples for the usage of Flexbox, Grid, Box alignment. For use in explaning through DevTools. Also has Shapes if there is time available to cover it.
---
+++ 58-cssconf-2019/gulpfile.babel.js
@@ -0,0 +1,56 @@
+const gulp        = require('gulp');
+const browserSync = require('browser-sync');
+const sass        = require('gulp-sass');
+const prefix      = require('gulp-autoprefixer');
+const cssnano     = require('gulp-cssnano');
+
+const startServer = (done) => {
+  browserSync.init({
+    server: "./",
+    port: 2314
+  })
+  done()
+}
+
+const browserSyncReload = (done) => {
+  browserSync.reload()
+  done()
+}
+
+const compileStyles = () => {
+  return gulp.src('scss/styles.scss')
+  .pipe(sass({
+    includePaths: ['scss'],
+    onError: browserSync.notify
+  }))
+  .pipe(prefix(['last 3 versions'], { cascade: true }))
+  .pipe(gulp.dest('./'))
+  .pipe(browserSync.reload({ stream:true }))
+}
+
+const watchMarkup = () => {
+  gulp.watch(['index.html'], browserSyncReload);
+}
+
+const watchStyles = () => { 
+  gulp.watch(['scss/*.scss'], compileStyles)
+}
+
+// Not exposed to CLI
+const serve = gulp.series(compileStyles, startServer)
+serve.description = 'serve compiled source on local server at port 3000'
+
+const watch = gulp.parallel(watchMarkup, watchStyles)
+watch.description = 'watch for changes to all source'
+
+const defaultTasks = gulp.parallel(serve, watch)
+
+export {
+  compileStyles,
+  serve,
+  watch,
+  watchMarkup,
+  watchStyles,
+}
+
+export default defaultTasks
