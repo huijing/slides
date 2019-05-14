14f43f045034e94e9dfb074ab493f4dd8cde7a0a
Chen Hui Jing <kakyou_tensai@yahoo.com>
2019-05-14 13:23:19 +0300

    Spruce up console log

---
+++ 58-cssconf-2019/js/custom.js
@@ -0,0 +1,15 @@
+const styles = [
+  'background: radial-gradient(rgba(255,255,255,0) 0, rgba(255,255,255,.15) 30%, rgba(255,255,255,.3) 32%, rgba(255,255,255,0) 33%) 0 0, radial-gradient(rgba(255,255,255,0) 0, rgba(255,255,255,.1) 11%, rgba(255,255,255,.3) 13%, rgba(255,255,255,0) 14%) 0 0, radial-gradient(rgba(255,255,255,0) 0, rgba(255,255,255,.2) 17%, rgba(255,255,255,.43) 19%, rgba(255,255,255,0) 20%) 0 110px, radial-gradient(rgba(255,255,255,0) 0, rgba(255,255,255,.2) 11%, rgba(255,255,255,.4) 13%, rgba(255,255,255,0) 14%) -130px -170px, radial-gradient(rgba(255,255,255,0) 0, rgba(255,255,255,.2) 11%, rgba(255,255,255,.4) 13%, rgba(255,255,255,0) 14%) 130px 370px, radial-gradient(rgba(255,255,255,0) 0, rgba(255,255,255,.1) 11%, rgba(255,255,255,.2) 13%, rgba(255,255,255,0) 14%) 0 0, linear-gradient(45deg, #343702 0%, #184500 20%, #187546 30%, #006782 40%, #0b1284 50%, #760ea1 60%, #83096e 70%, #840b2a 80%, #b13e12 90%, #e27412 100%)',
+  'background-size: 470px 470px, 970px 970px, 410px 410px, 610px 610px, 530px 530px, 730px 730px, 100% 100%',
+  'color: white',
+  'display: block',
+  'height: 60vh',
+  'line-height: 60vh',
+  'text-align: center',
+  'font-weight: bold',
+  'font-size: calc(1.5em + 1.5vw)'
+].join(';')
+
+const message = 'Using DevTools to understand modern CSS layouts'
+
+console.log('%c%s', styles, message)
