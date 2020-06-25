7f10b05bb1f17948d9e3ef32dfef94d88b66c646
huijing <huijing@hey.com>
2020-06-26 00:07:09 +0800

    Add moar bo liao stuff

--- 85-css-colours/js/custom.js
+++ 85-css-colours/js/custom.js
@@ -77,3 +77,43 @@ const toggleFullScreen = event => {
 
 document.body.addEventListener('keydown', controls, false)
 document.addEventListener('keydown', toggleFullScreen, false)
+
+
+const root = document.documentElement
+const colourInput = document.getElementById('colour')
+const hexCode = document.getElementById('hexCode')
+const hexDigits = document.querySelectorAll('[data-digit]')
+const hexValues = document.querySelectorAll('[data-value]')
+
+colourInput.addEventListener('input', () => { 
+  setRgb(colourInput.value)
+  let hexChars = colourInput.value.substring(1).split('')
+  hexCode.innerHTML = ''
+  for (var i = 0; i < hexValues.length; i++) {
+    hexValues[i].classList.remove('active')
+  }
+  hexChars.forEach(printHex)
+  hexChars.forEach(getCharacter)
+  hexCode.insertAdjacentHTML('afterbegin', '<span>#</span>')
+}, false)
+
+function setRgb(hex) {
+  const tempHex = hex.replace('#', '')
+  const r = parseInt(tempHex.substring(0, 2), 16)
+  const g = parseInt(tempHex.substring(2, 4), 16)
+  const b = parseInt(tempHex.substring(4, 6), 16)
+  
+  root.style.setProperty('--r', r)
+  root.style.setProperty('--g', g)
+  root.style.setProperty('--b', b)
+}
+
+function printHex(item, index, array) {
+  hexCode.innerHTML += '<span>' + item + '</span>'
+}
+
+function getCharacter(item, index, array) {
+  let set = hexDigits[index]
+  let value = set.querySelector('[data-value="' + item + '"]')
+  value.classList.add('active')
+}
--- 85-css-colours/scripts.js
+++ 85-css-colours/scripts.js
@@ -98,6 +98,44 @@ var toggleFullScreen = function toggleFullScreen(event) {
 
 document.body.addEventListener('keydown', controls, false);
 document.addEventListener('keydown', toggleFullScreen, false);
+var root = document.documentElement;
+var colourInput = document.getElementById('colour');
+var hexCode = document.getElementById('hexCode');
+var hexDigits = document.querySelectorAll('[data-digit]');
+var hexValues = document.querySelectorAll('[data-value]');
+colourInput.addEventListener('input', function () {
+  setRgb(colourInput.value);
+  var hexChars = colourInput.value.substring(1).split('');
+  hexCode.innerHTML = '';
+
+  for (var i = 0; i < hexValues.length; i++) {
+    hexValues[i].classList.remove('active');
+  }
+
+  hexChars.forEach(printHex);
+  hexChars.forEach(getCharacter);
+  hexCode.insertAdjacentHTML('afterbegin', '<span>#</span>');
+}, false);
+
+function setRgb(hex) {
+  var tempHex = hex.replace('#', '');
+  var r = parseInt(tempHex.substring(0, 2), 16);
+  var g = parseInt(tempHex.substring(2, 4), 16);
+  var b = parseInt(tempHex.substring(4, 6), 16);
+  root.style.setProperty('--r', r);
+  root.style.setProperty('--g', g);
+  root.style.setProperty('--b', b);
+}
+
+function printHex(item, index, array) {
+  hexCode.innerHTML += '<span>' + item + '</span>';
+}
+
+function getCharacter(item, index, array) {
+  var set = hexDigits[index];
+  var value = set.querySelector('[data-value="' + item + '"]');
+  value.classList.add('active');
+}
 "use strict";
 
 function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
