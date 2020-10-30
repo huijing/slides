ad9fcefc78e9d0ccb247a8f83a05e111386e68eb
huijing <huijing@hey.com>
2020-10-30 23:54:12 +0800

    Add jsconf mexico presentation

---
+++ 90-jsconfmx-2020/js/custom.js
@@ -0,0 +1,45 @@
+const style1 = [
+  'background: linear-gradient(324deg, lightgoldenrodyellow 4%, transparent 4%) -70px 43px, linear-gradient( 36deg, lightgoldenrodyellow 4%, transparent 4%) 30px 43px, linear-gradient( 72deg, gold 8.5%, transparent 8.5%) 30px 43px, linear-gradient(288deg, gold 8.5%, transparent 8.5%) -70px 43px, linear-gradient(216deg, gold 7.5%, transparent 7.5%) -70px 23px, linear-gradient(144deg, gold 7.5%, transparent 7.5%) 30px 23px, linear-gradient(324deg, lightgoldenrodyellow 4%, transparent 4%) -20px 93px, linear-gradient( 36deg, lightgoldenrodyellow 4%, transparent 4%) 80px 93px, linear-gradient( 72deg, gold 8.5%, transparent 8.5%) 80px 93px, linear-gradient(288deg, gold 8.5%, transparent 8.5%) -20px 93px, linear-gradient(216deg, gold 7.5%, transparent 7.5%) -20px 73px, linear-gradient(144deg, gold 7.5%, transparent 7.5%) 80px 73px;',
+  'background-color: lightgoldenrodyellow',
+  'background-size: 100px 100px',
+  'display: block',
+  'height: 50vh',
+  'text-align: center',
+  'line-height: 1.4',
+  'font-weight: bold',
+  'font-size: calc(2em + 2vw)',
+  'padding-top: 1.5em',
+  'padding-left: 0.5em',
+  'font-family: "Apple Color Emoji", "Segoe UI Emoji", "Input"'
+].join(';')
+
+const style2 = [
+  'background-color: white',
+  'background-image: linear-gradient(90deg, rgba(218, 112, 214, 0.15) 50%, transparent 50%),linear-gradient(rgba(218, 112, 214, 0.15) 50%, transparent 50%)',
+  'background-size: 25px 25px',
+  'font-size: calc(1em + 1.5vw)',
+  'display: block',
+  'text-align: center',
+  'padding: 1em',
+  'line-height:1.4'
+].join(';')
+
+const style3 = [
+  'background: repeating-linear-gradient(-45deg, transparent, transparent 1em, rgba(218, 112, 214, 0.4) 0, rgba(218, 112, 214, 0.1) 2rem, transparent 0, transparent 1rem, rgba(218, 112, 214, 0.3) 0, rgba(218, 112, 214, 0.1) 4rem, transparent 0, transparent 1rem, rgba(255, 255, 255, 0.3) 0, rgba(255, 255, 255, 0.1) 2rem), repeating-linear-gradient(45deg, transparent, transparent 1rem, rgba(218, 112, 214, 0.4) 0, rgba(218, 112, 214, 0.1) 2rem, transparent 0, transparent 1rem, rgba(218, 112, 214, 0.3) 0, rgba(218, 112, 214, 0.1) 4rem, transparent 0, transparent 1rem, rgba(255, 255, 255, 0.4) 0, rgba(255, 255, 255, 0.1) 2rem), #fff',
+  'background-blend-mode: multiply',
+  'color: white',
+  'font-size: calc(1.25em + 2vw)',
+  'display: block',
+  'text-align: center',
+  'padding: 0.5em',
+  'line-height: 1.4',
+  'font-family: "Apple Color Emoji", "Segoe UI Emoji"'
+].join(';')
+
+const message1 = 'Demystify Modern CSS Layouts with DevTools '
+const message2 = 'Chen Hui Jing | @hj_chen'
+const message3 = '🇲🇾👾🏀🚲🖌👟💻🖊🎙🦊🧗‍♀️🎹🏳️‍🌈'
+
+console.log('%c%s', style1, message1)
+console.log('%c%s', style2, message2)
+console.log('%c%s', style3, message3)
